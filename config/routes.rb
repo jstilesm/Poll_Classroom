5093856f6d64010e4b98d0e90e7d995c3534debe
NStilesMM <nicholas.mcdonnell@gmail.com>
2020-09-21 18:08:42 -0400

    Trial 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "staric_pages#root"
+  root "static_pages#root"
   
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :new, :show]
