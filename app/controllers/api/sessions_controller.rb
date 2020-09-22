7086e4777f9208ec1bdd53495df4cf8fbf005153
NStilesMM <nicholas.mcdonnell@gmail.com>
2020-09-22 11:48:47 -0400

    Heroku edit 1

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,11 +1,11 @@
 class Api::SessionsController < ApplicationController
     def create
         @user = User.find_by_credentials(
-            params[:user][:username]
-            params[:user][:password]
+            params[:user][:username],
+            params[:user][:password])
             # params[:user][:first_name]
             # params[:user][:last_name]
-        )
+        
         if @user
             login(@user)
             render # `/api/users/${user.id}`
