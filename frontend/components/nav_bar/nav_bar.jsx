e3751b0b08e73baf4e889e29c7c7f4b327bc56b5
NStilesMM <nicholas.mcdonnell@gmail.com>
2020-09-23 22:45:28 -0400

    Prepped for Heroku

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -5,12 +5,12 @@ const NavBar = ({currentUser, logout, testUser}) => {
     const test = { identifier: 'tester', password: 'password' };
     const sessionLink = () => (
         <nav className="login-signup">
-            <div className="button" >
+            {/* <div className="button" > */}
                 <button className="test-user" onClick={() => testUser(test)}>Test User</button>
                 <Link className="signup-link" to="/signup">Sign Up</Link>
                 <br/>
                 <Link className="login-link" to="/login">Log In</Link>
-            </div>
+            {/* </div> */}
         </nav>
 
     );
