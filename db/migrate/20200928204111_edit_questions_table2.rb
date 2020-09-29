161d9a59a6478266c9008302e517a2fe26e96342
NStilesMM <nicholas.mcdonnell@gmail.com>
2020-09-29 19:28:18 -0400

    Added more Questions Functionality, need more views still

--- db/migrate/20200928204111_edit_questions_table2.rb
+++
@@ -1,5 +0,0 @@
-class EditQuestionsTable2 < ActiveRecord::Migration[5.2]
-  def change
-    add_column :questions, :closed, :boolean, default: false,null: false
-  end
-end
--- db/migrate/20200929211949_edit_questions_table.rb
+++
@@ -1,8 +0,0 @@
-class EditQuestionsTable < ActiveRecord::Migration[5.2]
-  def change
-    remove_index :questions, :group_id
-    remove_column :questions, :group_id
-    add_column :questions, :group_id, :integer
-    add_index :questions, :group_id
-  end
-end
