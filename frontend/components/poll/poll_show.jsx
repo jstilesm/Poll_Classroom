d5df104e134ea4056d464002d228110ee7c48dc4
NStilesMM <nicholas.mcdonnell@gmail.com>
2020-11-13 12:13:47 -0500

    Set up links for heroku app

--- frontend/components/poll/poll_show.jsx
+++ frontend/components/poll/poll_show.jsx
@@ -28,10 +28,12 @@ class PollShow extends React.Component {
   render() {
     return (
       <div className="poll-page">
-        <img
-          className="PollEverywhere-blacklogo"
-          src="https://davhizrhxzcu1.cloudfront.net/assets/media_kit/logo_white-b3ae877b0dff730405738e5ad768060f7d6d56b89a75f397012ca915f5472364.png"
-        ></img>
+        <a href="/">
+          <img
+            className="PollEverywhere-blacklogo"
+            src="https://davhizrhxzcu1.cloudfront.net/assets/media_kit/logo_white-b3ae877b0dff730405738e5ad768060f7d6d56b89a75f397012ca915f5472364.png"
+          ></img>
+        </a>
 
         <div className="grey-logo"></div>
         <form onSubmit={this.handleSubmit} className="form-box-black">
